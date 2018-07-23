63f07c73d573f8fd4b94118abd7379857929dea8
leehanol <abyz1541@gmail.com>
2018-07-23 13:59:33 +0900

    home_position

--- proj_coop/back/20180710_home/20180710_home.ino
+++ proj_coop/back/20180710_home/20180710_home.ino
@@ -1,7 +1,6 @@
 
 #include <Motor.h>
 #include <MotorCtrl.h>
-#include <PhotoInterrupt.h>
 #include <AutoPID.h>
 #include <SPI.h>
 
@@ -13,6 +12,8 @@ double M2_Pos = 0, M2_SetPos = 0, M2_PWM_Val = 0;
 double M3_Pos = 0, M3_SetPos = 0, M3_PWM_Val = 0;
 
 double Pos[2];
+
+int home_m1 = 1, home_m2 = 1, home_m3 = 1;
 
 AutoPID M1_PID (&M1_Pos, &M1_SetPos, &M1_PWM_Val, M1_PWM_MIN, M1_PWM_MAX, M1_KP, M1_KI, M1_KD);
 AutoPID M2_PID (&M2_Pos, &M2_SetPos, &M2_PWM_Val, M2_PWM_MIN, M2_PWM_MAX, M2_KP, M2_KI, M1_KD);
@@ -26,11 +27,14 @@ void setup()
   Serial.begin(57600);
   Motor_Pin_Set(); PID_Set();
   Photo_Pin_Set();
-  Encoder_Interrupt_Set();
-  //SPI_DUE_SET();
   
-  M1_CTR.begin(); M2_CTR.begin(); M3_CTR.begin();
+  M1_CTR.begin();
+  M2_CTR.begin();
+  M3_CTR.begin();
 
+  delay(1000);
+
+  Encoder_Interrupt_Set();
 }
 
 void loop()
@@ -41,8 +45,8 @@ void loop()
     }
 
     // PID RUN
-    M1_PID.run(); PID_Val_Print(M1_Pos, M1_SetPos, M1_PWM_Val);
-    M2_PID.run(); //PID_Val_Print(M2_Pos, M2_SetPos, M2_PWM_Val);
+    M1_PID.run(); //PID_Val_Print(M1_Pos, M1_SetPos, M1_PWM_Val);
+    M2_PID.run(); PID_Val_Print(M2_Pos, M2_SetPos, M2_PWM_Val);
     M3_PID.run(); //PID_Val_Print(M3_Pos, M3_SetPos, M3_PWM_Val);
 
     Set_Real_Pos();
@@ -51,10 +55,15 @@ void loop()
     M2_CTR.Go_Motor(M2_MPWM,M2_DIRECTION,M2_PWM_Val);
     M3_CTR.Go_Motor(M3_MPWM,M3_DIRECTION,M3_PWM_Val);
 
-    
-    
     delay(1);
 }
+
+/*
+  Photo Interrupt Call Back
+*/
+void Photo_back_1(){digitalWrite(M1_STSP,LOW); M1_CTR.Call_back();}
+void Photo_back_2(){digitalWrite(M2_STSP,LOW); M2_CTR.Call_back();}
+void Photo_back_3(){digitalWrite(M3_STSP,LOW); M3_CTR.Call_back();}
 
 /*
   Encoder Interrupt Call Back
@@ -66,6 +75,16 @@ void M2_doEncoderB(){ M2_encoderPos += (digitalRead(M2_ENCODER_A)==digitalRead(M
 void M3_doEncoderA(){ M3_encoderPos += (digitalRead(M3_ENCODER_A)==digitalRead(M3_ENCODER_B))?1:-1;}
 void M3_doEncoderB(){ M3_encoderPos += (digitalRead(M3_ENCODER_A)==digitalRead(M3_ENCODER_B))?-1:1;}
 
+void Photo_Pin_Set()
+{
+   pinMode(Photo_1, INPUT_PULLUP);
+   pinMode(Photo_2, INPUT_PULLUP);
+   pinMode(Photo_3, INPUT_PULLUP);
+   attachInterrupt(Photo_1, Photo_back_1, FALLING);
+   attachInterrupt(Photo_2, Photo_back_2, FALLING);
+   attachInterrupt(Photo_3, Photo_back_3, FALLING);
+}
+ 
 void Encoder_Interrupt_Set()
 {
   attachInterrupt(M1_ENCODER_A, M1_doEncoderA, CHANGE);
@@ -104,3 +123,23 @@ void Set_Real_Pos()
 }
 
 
+/*
+void Home_Set()
+{
+  while(1)
+  {
+    digitalWrite(M1_DIRECTION, HIGH);
+    analogWrite(M1_MPWM,1000);
+      
+    if(home_m1==0)
+    {
+      break;
+    }
+  }
+
+  digitalWrite(M1_STSP, HIGH);
+  analogWrite(M1_MPWM,1000);
+  Serial.println("good");
+  
+}
+*/
